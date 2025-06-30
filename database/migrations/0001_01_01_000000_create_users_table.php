fe2ab9ae19b50cce9a4e32a0db9a49eebf9f7aea
GinaFerl <ginaferlindawati@gmail.com>
2025-06-30 09:26:32 +0700

    change the welcome & login display

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-             $table->string('username');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('profileImage')->nullable();
             $table->enum('role', ['superadmin', 'admin', 'board']);
+            $table->rememberToken();
             $table->timestamps();
         });
 
